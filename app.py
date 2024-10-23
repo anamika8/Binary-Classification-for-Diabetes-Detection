178d852b5802e00455227ff7de5bba6a597c063b
Anamika Nayak <40330477+anamika8@users.noreply.github.com>
2024-10-23 02:29:31 +0000

    adding .gitignore

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 '''
 with open('model.pkl', 'rb') as f:
     model = pickle.load(f)
-'''
+
 
 @app.route('/predict', methods=['POST'])
 def predict():
@@ -17,10 +17,11 @@ def predict():
     df = pd.DataFrame([data])
     prediction = model.predict(df)
     return jsonify({"prediction": int(prediction[0])})
+'''
 
 @app.route('/data', methods=['GET'])
 def data_head():
-    return load_dataset()
+    return load_dataset().to_json(orient='records')
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
