afe7dd4df428d09c32112c07de723d1d4829a632
Anamika Nayak <40330477+anamika8@users.noreply.github.com>
2024-10-23 03:06:00 +0000

    assert stratification

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 from flask import Flask, request, jsonify
 import pickle
 import pandas as pd
-from diabetes_detection_nn import load_dataset
+import json
+from diabetes_detection_nn import load_dataset, get_train_test_data
 
 app = Flask(__name__)
 
 # Load model
-'''
 with open('model.pkl', 'rb') as f:
     model = pickle.load(f)
 
@@ -17,11 +17,34 @@ def predict():
     df = pd.DataFrame([data])
     prediction = model.predict(df)
     return jsonify({"prediction": int(prediction[0])})
-'''
+
+@app.route('/assert', methods=['GET'])
+def assert_stratification():
+    X_train, X_test, y_train, y_test, y = get_train_test_data()
+    original_proportion = y.value_counts(normalize=True)
+    train_proportion = y_train.value_counts(normalize=True)
+    test_proportion = y_test.value_counts(normalize=True)
+
+    # Assert that the difference in proportions is small
+    assert (original_proportion - train_proportion).abs().max() < 0.05, "Training set stratification failed"
+    assert (original_proportion - test_proportion).abs().max() < 0.05, "Testing set stratification failed"
+
+    proportion_data = {
+    "original_proportion": original_proportion,
+    "train_proportion": train_proportion,
+    "test_proportion": test_proportion,
+    "message": "Assertion passed: The train and test sets are properly stratified." if ((abs(original_proportion - train_proportion) < 0.05) and (abs(original_proportion - train_proportion) < 0.05)) else "Proportion difference exceeds the threshold."
+    }
+
+    # Convert the dictionary to a JSON string
+    json_result = json.dumps(proportion_data, indent=4)
+
+    return json
+
 
 @app.route('/data', methods=['GET'])
 def data_head():
-    return load_dataset().to_json(orient='records')
+    return load_dataset().head().to_json(orient='records')
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
--- diabetes_detection_nn.py
+++ diabetes_detection_nn.py
@@ -1,4 +1,6 @@
 import pandas as pd
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
 
 def load_dataset():
     dataset_file_path = f'diabetes.csv'
@@ -6,7 +8,43 @@ def load_dataset():
     #reading data file into data frame
     df = pd.read_csv(dataset_file_path)
     df.columns = df.columns.str.strip()
-    return df.head()
+    return df
+
+def feature_scaling(df):
+    X = df.drop('diabetes', axis=1)
+    y = df['diabetes']
+
+    scaler = StandardScaler()
+
+    # Fit and transform the scaler on the features
+    X_scaled = scaler.fit_transform(X)
+    return X_scaled, y
+
+def preprocess_data(df):
+    columns_with_comma_decimal = ['chol_hdl_ratio', 'bmi', 'waist_hip_ratio']
+    for column in columns_with_comma_decimal:
+        df[column] = df[column].astype(str).str.replace(',', '.').astype(float)
+
+    # Encode the 'gender' column
+    df['gender'] = df['gender'].map({'male': 0, 'female': 1})
+
+    # Encode the 'diabetes' column
+    df['diabetes'] = df['diabetes'].map({'No diabetes': 0, 'Diabetes': 1})
+
+    # Handle Missing Values (if any)
+    df.fillna(df.median(), inplace=True)
+    scaled_data, y = feature_scaling(df)
+    return scaled_data, y
+
+def get_train_test_data():
+    scaled_data, y = preprocess_data(df=load_dataset())
+    '''
+    using stratify to split the data in such a way that the proportion of diabetes vs no diabetes cases in
+    both training and testing sets will match the proportion in the full dataset y.
+    '''
+    X_train, X_test, y_train, y_test = train_test_split(scaled_data, y, test_size=0.2, random_state=42, stratify=y)
+    return X_train, X_test, y_train, y_test, y
+
 
 if __name__ == "__main__":
     load_dataset()
