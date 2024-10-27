e37e753e93a974404ec84d1e9c8d07a4666ad37f
Anamika Nayak <40330477+anamika8@users.noreply.github.com>
2024-10-27 20:04:03 +0000

    changing the code to deploy

--- app_nb.py
+++ app_nb.py
@@ -7,12 +7,12 @@ import json
 from azure.storage.blob import BlobServiceClient
 from common import load_dataset
 
-app = Flask(__name__)
+app_nb = Flask(__name__)
 
-@app.route('/data-nb', methods=['GET'])
+@app_nb.route('/data-nb', methods=['GET'])
 def data_head():
     print("testing naive bayes model")
     return load_dataset().head().to_json(orient='records')
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    app_nb.run(host='0.0.0.0', port=5000)
